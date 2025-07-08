5eac7604ba8853e1546107b2ad8ac1378f004a34
Sportsman7101 <m.cericola06@gmail.com>
2025-07-08 10:28:14 -0400

    First commit

---
+++ chip8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "chip8.h"
+
+int main () {
+	uint8_t V0;
+		
+
+
+
+
+
+	return 0;
+}
+
---
+++ makefile
@@ -0,0 +1,7 @@
+build:
+	gcc chip8.c -o chip8
+
+run: 
+	./chip8
+
+
